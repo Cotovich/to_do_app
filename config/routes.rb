e075a91ed298a19be35853998e2d4854521f1565
Cotovich <reva.michael.p@gmail.com>
2022-08-04 19:32:12 +0300

    added sign up/in/out flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { registrations: "registrations" }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'lists#index'
+
+  devise_scope :user do
+    get '/users/sign_out' => 'sessions#destroy'
+    # get "/some/route" => "some_devise_controller"
+  end
 end
